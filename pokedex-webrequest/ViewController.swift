b55d4b20584208ef0ff9b3018dacb658c623d501
Prateek-Mahawar <prateekmahawar@gmail.com>
2016-07-09 00:23:10 +0530

    Search Bar Working

--- pokedex-webrequest/ViewController.swift
+++ pokedex-webrequest/ViewController.swift
@@ -7,18 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate , UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-
-    @IBOutlet weak var collection : UICollectionView!
+class ViewController: UIViewController, UICollectionViewDelegate , UICollectionViewDataSource, UICollectionViewDelegateFlowLayout,UISearchBarDelegate {
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var collection : UICollectionView!
+    var musicPlayer : AVAudioPlayer!
     var pokemon = [Pokemon]()
+    
+    var inSearchMode = false
+    var filteredPokemon = [Pokemon]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collection.delegate = self
-        collection.dataSource = self 
+        collection.dataSource = self
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.Done
+        
+        initAudio()
         parsePokemonCSV()
+        
+    }
+    
+    func initAudio(){
+        let path = NSBundle.mainBundle().pathForResource("music", ofType: "mp3")!
+        
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+        
+        } catch let err as NSError{
+            print(err.debugDescription)
+        }
     }
     
     func parsePokemonCSV(){
@@ -43,7 +67,13 @@ class ViewController: UIViewController, UICollectionViewDelegate , UICollectionV
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
-            let poke = pokemon[indexPath.row]
+            
+            let poke : Pokemon!
+            if inSearchMode{
+                poke = filteredPokemon[indexPath.row]
+            } else {
+                poke = pokemon[indexPath.row]
+            }
             cell.configureCell(poke)
             return cell
         } else {
@@ -58,7 +88,11 @@ class ViewController: UIViewController, UICollectionViewDelegate , UICollectionV
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 718
+        if inSearchMode {
+            return filteredPokemon.count
+        }
+            return pokemon.count
+        
     }
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -67,6 +101,42 @@ class ViewController: UIViewController, UICollectionViewDelegate , UICollectionV
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         return CGSizeMake(105, 105)
+    }
+    
+    @IBAction func musicBtnPressed(sender: UIButton!) {
+        if musicPlayer.playing {
+            musicPlayer.stop()
+            sender.alpha = 0.2
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+            
+        }
+        
+    }
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        collection.reloadData()
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearchMode = false
+            view.endEditing(true)
+            collection.reloadData()
+        } else {
+            inSearchMode = true
+            let lower = searchBar.text!.lowercaseString
+ 
+            filteredPokemon = pokemon.filter({$0.name.rangeOfString(lower) != nil})
+            collection.reloadData()
+            
+        }
+    }
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
 }
