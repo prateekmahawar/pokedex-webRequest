6b52c16eb05aa9bfcde15d5d1050bab18efae81b
Prateek-Mahawar <prateekmahawar@gmail.com>
2016-07-08 15:31:14 +0530

    Initial Commit

---
+++ pokedex-webrequest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-webrequest
+//
+//  Created by Prateek Mahawar on 08/07/16.
+//  Copyright © 2016 Prateek Mahawar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
