a05115353b196fc233030502951607fde6a267f0
Prateek-Mahawar <prateekmahawar@gmail.com>
2016-07-08 17:20:53 +0530

    Showing Images

--- pokedex-webrequest/PokeCell.swift
+++ pokedex-webrequest/PokeCell.swift
@@ -13,7 +13,12 @@ class PokeCell: UICollectionViewCell {
     @IBOutlet weak var nameLbl: UILabel!
     
     var pokemon : Pokemon!
-    
+   
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.cornerRadius =  5.0
+        
+    }
     func  configureCell(pokemon:Pokemon){
         self.pokemon = pokemon
         nameLbl.text = self.pokemon.name.capitalizedString
